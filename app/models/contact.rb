742dbfe248af404277a06f5f80389580afba4e2e
Edisontong <edisonjtong@gmail.com>
2023-01-16 12:02:58 -0700

    added contact class in the app/models folder

---
+++ app/models/contact.rb
@@ -0,0 +1,11 @@
+class Contact
+attr_accessor :first_name, :last_name, :phone_number, :email
+
+  def initialize(options)
+    @first_name = options[:first_name]
+    @last_name = options:[last_name]
+    @phone_number = options:[phone_number]
+    @email = options:[email]
+  end
+
+end
