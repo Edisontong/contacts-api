4195a0f10581779c71501533e374ccd47c880b0d
Edisontong <edisonjtong@gmail.com>
2023-01-16 12:59:14 -0700

    added rails datbase and migrate. deleted contact class previously created

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,2 @@
-class Contact
-  attr_accessor :first_name, :last_name, :phone_number, :email
-
-  def initialize(options)
-    @first_name = options[:first_name]
-    @last_name = options[:last_name]
-    @phone_number = options[:phone_number]
-    @email = options[:email]
-  end
+class Contact < ApplicationRecord
 end
